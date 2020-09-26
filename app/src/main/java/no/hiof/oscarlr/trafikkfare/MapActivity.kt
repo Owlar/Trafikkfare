352f2664b76a638a4bcff39fbf12ba75b1d6dd91
Owlar <oscarlr@hiof.no>
2020-09-26 02:21:37 +0200

    Add user location on map if user location request is granted. Don't show if user refuses

--- app/src/main/java/no/hiof/oscarlr/trafikkfare/MapActivity.kt
+++ app/src/main/java/no/hiof/oscarlr/trafikkfare/MapActivity.kt
@@ -1,18 +1,14 @@
 package no.hiof.oscarlr.trafikkfare
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
-import android.location.LocationManager.GPS_PROVIDER
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,30 +16,29 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_map.*
-import java.util.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     val HALDEN = LatLng(59.12478, 11.38754)
     val ZOOM_LEVEL = 13f
 
-    lateinit var locationManager : LocationManager
-    lateinit var location : Location
-
-    private lateinit var map : GoogleMap
+    lateinit var client : FusedLocationProviderClient
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var markerOptions : MarkerOptions
+    lateinit var latLng : LatLng
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        val mapFragment : SupportMapFragment? =
-            supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        mapFragment = (supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment)!!
+        mapFragment.getMapAsync(this)
+
+        client = LocationServices.getFusedLocationProviderClient(this)
 
         myLocation.setOnClickListener {
-            requestPermissionLocation()
+            getUserPosition()
         }
-
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
@@ -54,25 +49,32 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun requestPermissionLocation() {
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION), 15715)
+    private fun getUserPosition() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
+            client.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    mapFragment.getMapAsync {
+                        latLng = LatLng(location.latitude, location.longitude)
+                        markerOptions = MarkerOptions().position(latLng)
+                            .title("I am here")
+                        it.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL))
+                        it.addMarker(markerOptions)
 
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-        location = locationManager.getLastKnownLocation(GPS_PROVIDER)!!
-
-        var locationListener = LocationListener { p0 -> reverseGeocode(p0) }
-        locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 100.2f, locationListener)
+                        //val toast = Toast.makeText(this, location.latitude.toString() + ", " + location.longitude.toString(), LENGTH_LONG)
+                        //toast.show()
+                    }
+                }
+            }
+        }
+        else
+            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 11)
     }
 
-    private fun reverseGeocode(loc: Location) {
-        val geoCoder = Geocoder(this, Locale.getDefault())
-        val addresses = geoCoder.getFromLocation(loc.latitude, loc.longitude, 2)
-        val address = addresses[0]
-
-        val toast = Toast.makeText(this, address.latitude.toString() + ", " + address.longitude.toString(), LENGTH_LONG)
-        toast.show()
-
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 11) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED)
+                getUserPosition()
+        }
     }
 }
 
