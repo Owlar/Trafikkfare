8a97a19122ac322be873c8f807e84b7c943f4d95
Owlar <oscarlr@hiof.no>
2020-10-04 07:14:12 +0200

    Fix passing data between destinations, remove redundant buttons

--- app/src/main/java/no/hiof/oscarlr/trafikkfare/HomeFragment.kt
+++ app/src/main/java/no/hiof/oscarlr/trafikkfare/HomeFragment.kt
@@ -10,15 +10,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
+import no.hiof.oscarlr.trafikkfare.HomeFragmentArgs.Companion.fromBundle
 
 class HomeFragment : Fragment() {
 
     companion object {
         private const val SOMETHING_WENT_WRONG = "Something went wrong"
     }
+
+    private lateinit var usernameTextView : TextView
+    private lateinit var username : String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,26 +33,11 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         if (arguments != null) {
-            val usernameTextView: TextView = view.findViewById(R.id.textViewUsername)
-            val username = HomeFragmentArgs.fromBundle(requireArguments()).username
+            usernameTextView = textViewUsername
+            username = fromBundle(requireArguments()).username
             usernameTextView.text = username
-
-            userLocationButton.setOnClickListener {
-                val intent = Intent(activity, MapActivity::class.java)
-                startActivity(intent)
-            }
-
-            dangersButton.setOnClickListener {
-                val usernameFromTextView = textViewUsername.text.toString()
-
-                if (usernameFromTextView.isNotEmpty()) {
-                    val action = HomeFragmentDirections.actionSeeDangers(usernameFromTextView)
-                    it.findNavController().navigate(action)
-                }
-                else
-                    Toast.makeText(it.context, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
-            }
 
             bottom_navigation.setOnNavigationItemReselectedListener {
                 navigateToDistOf(it)
@@ -60,17 +48,45 @@ class HomeFragment : Fragment() {
     private fun navigateToDistOf(item: MenuItem) {
         when (item.itemId) {
             R.id.home_dest -> {
-                findNavController().navigate(R.id.homeFragment)
-                Log.d("MainActivity", "Navigated to home")
+                navigateToHome()
             }
             R.id.dangers_dest -> {
-                findNavController().navigate(R.id.dangers)
+                navigateToDangers()
             }
             R.id.map_dest -> {
-                val intent = Intent(activity, MapActivity::class.java)
-                startActivity(intent)
-                Log.d("MapActivity", "Navigated to map")
+                navigateToMap()
             }
         }
+    }
+
+    private fun navigateToHome() {
+        val usernameFromTextView = textViewUsername.text.toString()
+
+        if (usernameFromTextView.isNotEmpty()) {
+            val action = HomeFragmentDirections.actionSeeHome(usernameFromTextView)
+            findNavController().navigate(action)
+        }
+        else
+            Toast.makeText(context, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
+
+        Log.d("MainActivity", "Navigated to home")
+    }
+
+    private fun navigateToDangers() {
+        val usernameFromTextView = textViewUsername.text.toString()
+
+        if (usernameFromTextView.isNotEmpty()) {
+            val action = HomeFragmentDirections.actionSeeDangers(usernameFromTextView)
+            findNavController().navigate(action)
+        }
+        else
+            Toast.makeText(context, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
+        Log.d("MainActivity", "Navigated to dangers")
+    }
+
+    private fun navigateToMap() {
+        val intent = Intent(activity, MapActivity::class.java)
+        startActivity(intent)
+        Log.d("MapActivity", "Navigated to map")
     }
 }
