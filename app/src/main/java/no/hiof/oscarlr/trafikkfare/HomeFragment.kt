5e55284a54a864adf39e3e8635d456e15ec6baa8
Owlar <oscarlr@hiof.no>
2020-10-13 21:41:55 +0200

    Refactor bottom navigation bar to activity

--- app/src/main/java/no/hiof/oscarlr/trafikkfare/HomeFragment.kt
+++ app/src/main/java/no/hiof/oscarlr/trafikkfare/HomeFragment.kt
@@ -17,11 +17,14 @@ import no.hiof.oscarlr.trafikkfare.HomeFragmentArgs.Companion.fromBundle
 class HomeFragment : Fragment() {
 
     companion object {
-        private const val SOMETHING_WENT_WRONG = "Something went wrong"
+        private lateinit var usernameTextView : TextView
+        private lateinit var username : String
+
+        fun getUsernameTextView(): TextView {
+            return usernameTextView
+        }
     }
 
-    private lateinit var usernameTextView : TextView
-    private lateinit var username : String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,55 +41,8 @@ class HomeFragment : Fragment() {
             usernameTextView = textViewUsername
             username = fromBundle(requireArguments()).username
             usernameTextView.text = username
-
-            bottom_navigation.setOnNavigationItemReselectedListener {
-                navigateToDistOf(it)
-            }
         }
     }
 
-    private fun navigateToDistOf(item: MenuItem) {
-        when (item.itemId) {
-            R.id.home_dest -> {
-                navigateToHome()
-            }
-            R.id.dangers_dest -> {
-                navigateToDangers()
-            }
-            R.id.map_dest -> {
-                navigateToMap()
-            }
-        }
-    }
 
-    private fun navigateToHome() {
-        val usernameFromTextView = textViewUsername.text.toString()
-
-        if (usernameFromTextView.isNotEmpty()) {
-            val action = HomeFragmentDirections.actionSeeHome(usernameFromTextView)
-            findNavController().navigate(action)
-        }
-        else
-            Toast.makeText(context, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
-
-        Log.d("MainActivity", "Navigated to home")
-    }
-
-    private fun navigateToDangers() {
-        val usernameFromTextView = textViewUsername.text.toString()
-
-        if (usernameFromTextView.isNotEmpty()) {
-            val action = HomeFragmentDirections.actionSeeDangers(usernameFromTextView)
-            findNavController().navigate(action)
-        }
-        else
-            Toast.makeText(context, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
-        Log.d("MainActivity", "Navigated to dangers")
-    }
-
-    private fun navigateToMap() {
-        val intent = Intent(activity, MapActivity::class.java)
-        startActivity(intent)
-        Log.d("MapActivity", "Navigated to map")
-    }
 }
--- app/src/main/java/no/hiof/oscarlr/trafikkfare/MainActivity.kt
+++ app/src/main/java/no/hiof/oscarlr/trafikkfare/MainActivity.kt
@@ -1,13 +1,98 @@
 package no.hiof.oscarlr.trafikkfare
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val SOMETHING_WENT_WRONG = "Something went wrong"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navController = findNavController(R.id.nav_host_fragment)
+        setupBottomNavMenu(navController)
+
+        /*top_app_bar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.profile -> {
+                    true
+                }
+                R.id.search -> {
+                    true
+                }
+                R.id.more -> {
+                    true
+                }
+                else -> false
+            }
+        }*/
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav?.setupWithNavController(navController)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_dest -> {
+                    navigateToHome()
+                }
+                R.id.dangers_dest -> {
+                    navigateToDangers()
+                }
+                R.id.map_dest -> {
+                    navigateToMap()
+                }
+            }
+            true
+        }
+    }
+
+    private fun navigateToHome() {
+        val usernameFromTextView = HomeFragment.getUsernameTextView().text.toString()
+
+        if (usernameFromTextView.isNotEmpty()) {
+            val action = HomeFragmentDirections.actionSeeHome(usernameFromTextView)
+            findNavController(R.id.nav_host_fragment).navigate(action)
+        }
+        else
+            Toast.makeText(this, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
+
+        Log.d("MainActivity", "Navigated to home")
+    }
+
+    private fun navigateToDangers() {
+        val usernameFromTextView = HomeFragment.getUsernameTextView().text.toString()
+
+        if (usernameFromTextView.isNotEmpty()) {
+            val action = HomeFragmentDirections.actionSeeDangers(usernameFromTextView)
+            this.findNavController(R.id.nav_host_fragment).navigate(action)
+        }
+        else
+            Toast.makeText(this, SOMETHING_WENT_WRONG, Toast.LENGTH_LONG).show()
+        Log.d("MainActivity", "Navigated to dangers")
+    }
+
+    private fun navigateToMap() {
+        val intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
+        Log.d("MapActivity", "Navigated to map")
     }
 }
