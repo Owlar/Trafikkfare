f7eb725d00a3e05a65685c86ee0fb50a652c31d8
Owlar <oscarlr@hiof.no>
2020-11-03 20:32:41 +0100

    Add check that both title & description are set before saving

--- app/src/main/java/no/hiof/oscarlr/trafikkfare/EditDangerModalFragment.kt
+++ app/src/main/java/no/hiof/oscarlr/trafikkfare/EditDangerModalFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_edit_danger.*
 import kotlinx.android.synthetic.main.fragment_edit_danger.view.*
@@ -26,11 +27,15 @@ class EditDangerModalFragment : BottomSheetDialogFragment() {
         val saveButton = view.findViewById<Button>(R.id.dangerSaveButton)
         setText(view)
         saveButton.setOnClickListener {
-            editDangerBottomSheetListener.saveDangerButtonClicked(
-                editDangerTitle.text.toString(),
-                editDangerDescription.text.toString()
-            )
-            dismiss()
+            if (editDangerTitle.text.toString().isNotEmpty() && editDangerDescription.text.toString().isNotEmpty()) {
+                editDangerBottomSheetListener.saveDangerButtonClicked(
+                    editDangerTitle.text.toString(),
+                    editDangerDescription.text.toString()
+                )
+                dismiss()
+            }
+            else
+                Toast.makeText(it.context, "Please fill in title and description", Toast.LENGTH_LONG).show()
         }
 
         return view
